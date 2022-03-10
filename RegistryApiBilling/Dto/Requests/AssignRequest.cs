1b733b402b5a9a7efda2ae7ed4b7677248620551
BlackNord <technic_pr@mail.ru>
2022-03-10 10:52:24 +0300

    registry-api-billing - dtos were added

---
+++ RegistryApiBilling/Dto/Requests/AssignRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Registry.Api.Billing.Dto.Requests
+{
+	public class AssignRequest
+	{
+		public int AssignId { get; set; }
+	}
+}
---
+++ RegistryApiBilling/Dto/Responses/AssignResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Registry.Api.Billing.Dto.Responses
+{
+	public class AssignResponse
+	{
+		public bool IsAllowed { get; set; }
+
+		public string? RejectReason { get; set; }
+	}
+}
