ff8bd0022844afbb8830ea12884ac843348e6e97
BlackNord <technic_pr@mail.ru>
2022-03-10 10:55:12 +0300

    registry-api-billing - services were added

---
+++ RegistryApiBilling/Services/BillingService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Options;
+using Registry.Api.Billing.Dto.Requests;
+using Registry.Api.Billing.Dto.Responses;
+using Registry.Api.Billing.Integratiion;
+using Registry.Api.Billing.Tools;
+
+using Google.Cloud.PubSub.V1;
+using Newtonsoft.Json;
+
+namespace Registry.Api.Billing.Services.Interfaces
+{
+	public class BillingService : IBillingService
+	{
+		private readonly GCSettings settings;
+
+		public BillingService(IOptions<GCSettings> settings)
+		{
+			this.settings = settings.Value;
+		}
+
+		public async Task<AssignResponse> Assign(AssignRequest request)
+		{
+			var result = new AssignResponse
+			{
+				IsAllowed = BooleanGenerator.NextBoolean(),
+				RejectReason = "Reject reason"
+			};
+
+			var topic = TopicName.FromProjectTopic(settings.ProjectId, settings.PublishId);
+			var publisher = await PublisherClient.CreateAsync(topic);
+
+			var message = JsonConvert.SerializeObject(result);
+			await publisher.PublishAsync(message);
+
+			return result;
+		}
+	}
+}
